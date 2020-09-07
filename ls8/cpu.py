2a798a6ebbd8b02616825b4607463a16d2ff297c
smv5047 <smv5047@gmail.com>
2020-09-07 15:58:00 -0400

    working on cpu load

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,11 +1,6 @@
 """CPU functionality."""
-
+# TODO Why are my instructions not being interpretted correctly
 import sys
-
-# set instruction codes
-HLT = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
 
 
 class CPU:
@@ -20,33 +15,50 @@ class CPU:
         self.register = [0] * 8
         # program counter
         self.pc = 0
+        # ops codes
+        # set of instruction codes
+        self.ops = {
+            'HLT': 0b00000001,  # Halt
+            'LDI': 0b10000010,
+            'PRN': 0b01000111  # Print
+        }
 
     def load(self):
         """Load a program into memory."""
-
+        # open a file
+        # get file name from command line arguments
+        # argv gives us an array of arguments passed in via the command line
+        # check if not enough or too many arguments were past
         address = 0
+        if len(sys.argv) != 2:
+            print("Usage: failed to pass correct number of arguments")
+            sys.exit(1)
+        try:
+            # LOAD FILE FROM SYS.ARGV[1]
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    # split the current lin on # symbol
+                    split_line = line.split('#')
+                    # removes whitespace and \n character
+                    code_value = split_line[0].strip()
+                    # make sure value before # is nto empty
+                    if code_value == '':
+                        continue
 
-        # For now, we've just hardcoded a program:
-        # memory array
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+                    num = int(code_value)
+                    self.ram[address] = num
+                    address += 1
+                    # print(bin(int(line)))
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[1]} file not found")
+            sys.exit(1)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -67,7 +79,7 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
     # Inside the CPU, there are two internal registers used for memory operations:
@@ -80,28 +92,32 @@ class CPU:
     def ram_read(self, MAR):
         return self.ram[MAR]
 
-    def ram_write(self):
+    def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
     def run(self):
         """Run the CPU."""
-        pass
         while self.is_running:
             instruction = self.ram_read(self.pc)
 
             operand_a = self.ram_read(self.pc+1)
             operand_b = self.ram_read(self.pc+2)
 
-            if instruction == LDI:
+            if instruction == self.ops['LDI']:
 
                 self.register[operand_a] = operand_b
                 self.pc += 3
 
-            elif instruction == PRN:
+            elif instruction == self.ops['PRN']:
 
                 print(self.register[operand_a])
                 self.pc += 2
 
-            elif instruction == HLT:
+            elif instruction == self.ops['HLT']:
                 self.running = False
                 self.pc += 1
+
+            else:
+                print(f"Unknown instruction {instruction}")
+                # program did not end cleanly
+                sys.exit(1)
